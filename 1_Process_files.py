b47c41e15ecf5fb7dfad1bc4cc4c9aaa0d912856
Dorothea <dorothea.french@gmail.com>
2025-05-01 18:40:02 -0600

    finished 1-1

--- 1_Process_files.py
+++ 1_Process_files.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import align_prepare_transcripts
 
-import nltk
-nltk.download('punkt_tab')
+# import nltk
+# nltk.download('punkt_tab')
 
 def seperate_by_timing(dataframe_source, file_path, name, split_file):
     dataframe = dataframe_source.copy()
